f77dc0efaaaa8c7ee7185286bfcea0baedbb339b
Yavuz <app22@Yavuzs-MacBook-Air.local>
2022-10-16 12:59:19 +0300

    view controller

---
+++ ViewController/ViewController/IkinciViewController.swift
@@ -0,0 +1,22 @@
+//
+//  IkinciViewController.swift
+//  ViewController
+//
+//  Created by Yavuz on 16.10.2022.
+//
+
+import UIKit
+
+class IkinciViewController: UIViewController {
+    @IBOutlet weak var ikinciLabel: UILabel!
+    
+    @IBOutlet weak var bulunan: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ ViewController/ViewController/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by Yavuz on 16.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var birinciLabel: UILabel!
+    
+    @IBOutlet weak var text: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func kontrol(_ sender: Any) {
+    }
+    
+}
+
